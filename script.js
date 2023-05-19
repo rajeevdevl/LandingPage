23bf4254c7b86477e2e3298d8e96827f36b9be41
Rajeev <rajeevkr.developer@gmail.com>
2023-05-19 13:05:45 +0530

    changes

--- script.js
+++ script.js
@@ -68,8 +68,8 @@ const textArray = [
   "Embed Smart Banners",
   "Automate Remediation Process",
 ];
-const typingDelay = 150;
-const erasingDelay = 150;
+const typingDelay = 100;
+const erasingDelay = 100;
 const newTextDelay = 100; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
